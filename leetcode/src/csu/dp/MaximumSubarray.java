aef53f93956965de4ad0b6629ea45a3a3a30fc77
csupp <596396296@qq.com>
2015-06-11 17:24:09 +0800

    Maximum Subarray

---
+++ leetcode/src/csu/dp/MaximumSubarray.java
@@ -0,0 +1,23 @@
+package csu.dp;
+
+public class MaximumSubarray {
+	public static int maxSubArray(int[] nums) {
+		int curMax = 0;
+		int max = nums[0];
+		for (int i = 0; i < nums.length; i++) {
+			curMax += nums[i];
+			if (curMax > max) {
+				max = curMax;
+			}
+			if (curMax < 0) {
+				curMax = 0;
+			}
+		}
+		return max;
+	}
+
+	public static void main(String[] args) {
+		int[] nums = { -2, -1 };
+		System.out.println(maxSubArray(nums));
+	}
+}
